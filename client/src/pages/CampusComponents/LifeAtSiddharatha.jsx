f413d7cf3830487e384b4eccd5355fb01594957f
Anvesh_1130 <anveshkankanala1130@gmail.com>
2025-04-15 15:03:54 +0530

    Revert "Anvesh branch"

--- client/src/pages/CampusComponents/LifeAtSiddharatha.jsx
+++ client/src/pages/CampusComponents/LifeAtSiddharatha.jsx
@@ -3,95 +3,32 @@ import { Carousel } from "react-responsive-carousel";
 import "react-responsive-carousel/lib/styles/carousel.min.css";
 import { motion } from "framer-motion";
 import AboutHeader from "../../ResuableComponents/AboutHeading";
-import { FaRegCommentDots } from "react-icons/fa";
-import {
-  FaTools,
-  FaUserGraduate,
-  FaUsers,
-  FaRegNewspaper,
-} from "react-icons/fa";
-import { MdOutlineSupportAgent } from "react-icons/md";
-import { GiNetworkBars } from "react-icons/gi";
-import { HiOutlineAcademicCap } from "react-icons/hi";
-import { BsPencilSquare } from "react-icons/bs";
+import FacilitiesAtSiddharatha from "./FacilitiesAtSiddharatha";
+import TransportRoutes from "./TransportRoutes";
 
 const LifeAtSiddhartha = () => {
-  const cardData = [
-    {
-      title: "Skill Enhancement",
-      description:
-        "GNITS fosters a supportive and inclusive environment that empowers women to thrive in technology and engineering, providing the confidence and skills needed for leadership and success.",
-      icon: <FaTools />,
-      // bgImage: "url('images/student/img1.jpeg')",
-    },
-    {
-      title: "Student Counseling",
-      description:
-        "GNITS offers state-of-the-art labs and research centers, giving students the opportunity to engage in hands-on learning and innovate in emerging fields like AI, VLSI, and more.",
-      icon: <MdOutlineSupportAgent />,
-      // bgImage: "url('images/student/img2.jpeg')",
-    },
-    {
-      title: "Student Chapters",
-      description:
-        "Our curriculum is developed with direct industry input, ensuring students gain cutting-edge, relevant skills that prepare them for dynamic and successful global careers.",
-      icon: <FaUsers />,
-      // bgImage: "url('images/student/img3.jpeg')",
-    },
-    {
-      title: "EDC Cell",
-      description:
-        "Our dedicated placement cell works to connect students with top employers, offering career guidance and ensuring excellent job opportunities in prestigious companies worldwide.",
-      icon: <GiNetworkBars />,
-      // bgImage: "url('images/student/img4.jpeg')",
-    },
-    {
-      title: "NPTEL",
-      description:
-        "GNITS encourages students to grow beyond academics through leadership programs, extracurricular activities, and skill-building workshops, fostering well-rounded personal and professional development.",
-      icon: <HiOutlineAcademicCap />,
-      // bgImage: "url('images/student/img5.jpeg')",
-    },
-    {
-      title: "EC Council Academia",
-      description:
-        "GNITS encourages students to grow beyond academics through leadership programs, extracurricular activities, and skill-building workshops, fostering well-rounded personal and professional development.",
-      icon: <FaUserGraduate />,
-      // bgImage: "url('images/student/img6.jpeg')",
-    },
-    {
-      title: "Newsletter",
-      description:
-        "GNITS offers state-of-the-art labs and research centers, giving students the opportunity to engage in hands-on learning and innovate in emerging fields like AI, VLSI, and more.",
-      icon: <FaRegNewspaper />,
-      // bgImage: "url('images/student/img7.jpeg')",
-    },
-    {
-      title: "Student Blog",
-      description:
-        "GNITS encourages students to grow beyond academics through leadership programs, extracurricular activities, and skill-building workshops, fostering well-rounded personal and professional development.",
-      icon: <BsPencilSquare />,
-      // bgImage: "url('images/student/img8.jpeg')",
-    },
-  ];
   return (
     <>
       <AboutHeader
         title="Campus Life"
         image="https://universityinnovation.org/images/4/4f/Depvr.JPG"
       />
-      <div className="px-4 md:px-10 py-10 md:py-20 bg-gray-100">
-        <div className="bg-white rounded-2xl shadow-2xl p-6 md:p-12 flex flex-col md:flex-row justify-between items-center gap-10">
+
+      <div className="px-4 md:px-10 py-10 bg-gray-100">
+        <div className="bg-white rounded-2xl shadow-2xl p-6 md:p-10 flex flex-col md:flex-row justify-between items-center gap-10">
+          {/* Content Section */}
           {/* Content Section */}
           <div className="md:w-1/2">
             <motion.h2
-              className="text-5xl font-serif font-semibold text-[#003f3c] mb-6"
-              initial={{ opacity: 0, y: -20 }}
-              animate={{ opacity: 1, y: 0 }}
+              className="text-3xl md:text-4xl font-bold mb-4 pt-2"
+              initial={{ opacity: 0, y: 40 }}
+              whileInView={{ opacity: 1, y: 0 }}
               transition={{ duration: 0.8, ease: "easeOut" }}
+              viewport={{ once: true, amount: 0.6 }}
             >
               Life At <span className="text-[#005f57]">SIDDHARTHA</span>
             </motion.h2>
+
             <p className="text-lg leading-relaxed text-gray-700">
               Life at SIDDHARTHA is dynamic and inspiring, designed to empower
               students with a rich learning environment and holistic
@@ -139,80 +76,9 @@ const LifeAtSiddhartha = () => {
           </div>
         </div>
       </div>
-      {/* Holistic Grievance Redressal */}
-      <div className="bg-[#003F41] text-white py-16 px-6 md:px-24">
-        <div className="flex flex-col md:flex-row items-start justify-between gap-16">
-          {/* Left Section */}
-          <div className="w-full md:w-2/3">
-            <h2 className="text-3xl md:text-4xl font-bold mb-6">
-              Holistic Grievance Redressal
-            </h2>
-            <p className="text-sm md:text-base font-medium leading-relaxed">
-              At GNITS, we are committed to fostering a supportive and inclusive
-              environment for our students, faculty, and staff. Our holistic
-              grievance redressal approach ensures that every concern is
-              addressed promptly and fairly, promoting a culture of mutual
-              respect and harmony. By integrating transparent processes and
-              responsive measures, we aim to create an atmosphere where every
-              member of our community feels valued and heard, thereby
-              contributing to the overall well-being and academic excellence of
-              the institution.
-            </p>
-          </div>
 
-          {/* Right Section */}
-          <div className="w-full md:w-1/4 flex flex-col items-center justify-center text-center">
-            <h3 className="text-sm md:text-xl font-semibold mb-4 whitespace-nowrap">
-              Online Grievance Redressal
-            </h3>
-            <button className="bg-[#D98C00] text-white font-semibold px-6 py-3 rounded flex items-center gap-2 shadow-lg hover:bg-[#c87f00] transition">
-              <FaRegCommentDots size={20} />
-              Register Grievance
-            </button>
-          </div>
-        </div>
-      </div>
-      {/* students corner */}
-      <div className="min-h-screen flex items-center justify-center bg-gray-100 px-15 py-5">
-        <div className="w-full max-w-7xl">
-          <h1 className="text-3xl font-bold mb-4 ">Students Corner</h1>
-          <p className=" mb-8 ">
-            GNITS empowers women to excel in technology through industry-aligned
-            education, advanced resources, holistic development, and strong
-            placement support. Unlock your potential and shape the future with
-            GNITS.
-          </p>
-          <div
-            className={`grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6 ${
-              cardData.length % 3 === 2 ? "lg:grid-cols-2" : ""
-            } py-15`}
-          >
-            {cardData.map((card, index) => (
-              <div
-                key={index}
-                className="p-6 bg-[#003F41]   rounded-lg shadow-md hover:shadow-lg transition relative overflow-hidden flex flex-col justify-between items-start min-h-[350px] abute inset-0  hover:bg-gradient-to-r from-green-400 to-blue-500 hover:duration-300 "
-                style={{
-                  backgroundImage: card.bgImage,
-                  backgroundSize: "cover",
-                }}
-              >
-                {/* Gradient Hover Effect */}
-                <div className="absolute inset-0 bg-gradient-to-r from-green-400 to-blue-500 opacity-0 hover:opacity-80 transition-opacity duration-300"></div>
-                <div className="relative z-10">
-                  <div className="text-4xl text-green-600 mb-4">
-                    {card.icon}
-                  </div>
-                  <h2 className="text-lg font-semibold text-white mb-2">
-                    {card.title}
-                  </h2>
-                  <div className="w-full h-0.5 bg-gray-300 my-2"></div>
-                  <p className="text-white">{card.description}</p>
-                </div>
-              </div>
-            ))}
-          </div>
-        </div>
-      </div>
+      <FacilitiesAtSiddharatha />
+      <TransportRoutes />
     </>
   );
 };
